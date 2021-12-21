023e8f0ecfc9ec64822dc160d5c494807a90a291
jamestheadora <87346916+jamestheadora@users.noreply.github.com>
2021-12-21 01:35:48 -0500

    Updated

---
+++ Mission_to_Mars/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+#Initialize flask app
+app = Flask(__name__)
+
+#set up Mongo connection
+mongo  = PyMongo(app, uri="mongodb://localhost:27017/Mars_DB")
+
+@app.route("/")
+def index():
+    # access information from the database
+    mars_data = mongo.db.collection.find_one()
+    #Display the home page
+    print(f"======== {mars_data}")
+    return render_template("index.html", mdata = mars_data)
+
+@app.route("/scrape")
+def scrape():
+    #call scrape mars script
+    mars_data = scrape_mars.scrape_info()
+    data = scrape_mars.scrape_info()
+    print(data)
+
+    # Take dictionary and load into mongoDB     
+    mongo.db.collection.insert_one(data)
+
+    # Redirect back to the home page/index route
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
